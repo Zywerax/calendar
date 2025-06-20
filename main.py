0b4e5257540d0523a2e2bd7fdf7723befe98cb48
Zywerax <117526536+Zywerax@users.noreply.github.com>
2025-06-20 13:02:10 +0200

    feat: new endpoint - modify task

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ def get_db():
     finally:
         db.close()
 
+"Add task with title and status"
 @app.post("/tasks", response_model=schemas.Task)
 def create_task(task: schemas.TaskCreate, db: Session = Depends(get_db)):
     db_task = models.Task(title=task.title, done=task.done)
@@ -23,6 +24,21 @@ def create_task(task: schemas.TaskCreate, db: Session = Depends(get_db)):
     db.refresh(db_task)
     return db_task
 
+"Read whole tasks from db"
 @app.get("/tasks", response_model=list[schemas.Task])
 def read_tasks(db: Session = Depends(get_db)):
     return db.query(models.Task).all()
+
+"Modify task"
+@app.put("/tasks/{task_id}", response_model=schemas.Task)
+def update_task(task_id: int, updated_task: schemas.TaskCreate, db: Session = Depends(get_db)):
+    db_task = db.query(models.Task).filter(models.Task.id == task_id).first()
+    if db_task is None: 
+        raise HTTPException(status_code=404, detail="Task not found")
+    
+    db_task.title = updated_task.title
+    db_task.done = updated_task.done
+
+    db.commit()
+    db.refresh(db_task)
+    return db_task
