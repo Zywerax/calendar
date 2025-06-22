1d0aa508c02f018f4e80517d5eecd2e0b0719011
Zywerax <117526536+Zywerax@users.noreply.github.com>
2025-06-22 17:13:01 +0200

    feat: new endpoint-get task by id

---
+++ app/crud/task.py
@@ -0,0 +1,66 @@
+from sqlalchemy.orm import Session
+from fastapi import Depends, HTTPException, Response
+from app.database import get_db
+import app.models.task as task_model
+import app.schemas.task as task_schemas
+
+
+"Add task with title and status"
+def create_task(task: task_schemas.TaskCreate, db: Session = Depends(get_db)):
+    db_task = task_model.Task(title=task.title, done=task.done)
+    db.add(db_task)
+    db.commit()
+    db.refresh(db_task)
+    return db_task
+
+"""Read whole tasks from db"""
+def read_tasks(db: Session = Depends(get_db)):
+    return db.query(task_model.Task)\
+            .filter(task_model.Task.deleted == False)\
+            .all()
+
+"""Read whole tasks from db"""
+def read_task_by_id(task_id: int, db: Session = Depends(get_db)):
+    return db.query(task_model.Task)\
+            .filter(task_model.Task.deleted == False)\
+            .filter(task_model.Task.id == task_id)\
+            .first()
+
+"""Modify whole task by id"""
+def update_task(task_id: int, updated_task: task_schemas.TaskCreate, db: Session = Depends(get_db)):
+    db_task = db.query(task_model.Task).filter(task_model.Task.id == task_id).first()
+    if db_task is None: 
+        raise HTTPException(status_code=404, detail="Task not found")
+    
+    db_task.title = updated_task.title
+    db_task.done = updated_task.done
+
+    db.commit()
+    db.refresh(db_task)
+    return db_task
+
+"""Delete task by marking it as deleted"""
+def delete_task(task_id: int, db: Session = Depends(get_db)):
+    db_task = db.query(task_model.Task).filter(task_model.Task.id == task_id).first()
+    if db_task is None:
+        raise HTTPException(status_code=404, detail="Task not found")
+
+    db_task.deleted = True  # Mark as deleted instead of removing from DB
+    db.commit()
+    db.refresh(db_task)
+    return Response(status_code=204)
+
+"""Modify part of task by id"""
+def patch_task(task_id: int, updated_task: task_schemas.TaskUpdate, db: Session = Depends(get_db)):
+    db_task = db.query(task_model.Task).filter(task_model.Task.id == task_id).first()
+    if db_task is None:
+        raise HTTPException(status_code=404, detail="Task not found")
+
+    if updated_task.title is not None:
+        db_task.title = updated_task.title
+    if updated_task.done is not None:
+        db_task.done = updated_task.done
+
+    db.commit()
+    db.refresh(db_task)
+    return db_task
---
+++ app/routers/task.py
@@ -0,0 +1,41 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app.database import get_db
+import app.models.task as task_model
+import app.schemas.task as task_schemas
+import app.crud.task as task_crud
+
+router = APIRouter()
+
+"""Add task with title and status"""
+@router.post("/tasks", response_model=task_schemas.Task)
+def create_task(task: task_schemas.TaskCreate, db: Session = Depends(get_db)):
+    return task_crud.create_task(task=task, db=db)
+
+"""Read whole tasks from db"""
+@router.get("/tasks", response_model=list[task_schemas.Task])
+def read_tasks(db: Session = Depends(get_db)):
+    return task_crud.read_tasks(db=db)
+
+"""Read task by if not returns 404"""
+@router.get("/tasks/{task_id}", response_model=task_schemas.Task)
+def read_task_by_id(task_id: int, db: Session = Depends(get_db)):
+    task = task_crud.read_task_by_id(task_id=task_id, db=db)
+    if not task:
+        raise HTTPException(status_code=404, detail="Task not found")
+    return task
+
+"""Delete task by marking it as deleted"""
+@router.delete("/tasks/{task_id}", response_model=task_schemas.Task)
+def delete_task(task_id: int, db: Session = Depends(get_db)):
+    return task_crud.delete_task(task_id=task_id, db=db)
+
+"""Modify whole task by id"""
+@router.put("/tasks/{task_id}", response_model=task_schemas.Task)
+def update_task(task_id: int, updated_task: task_schemas.TaskCreate, db: Session = Depends(get_db)):
+    return task_crud.update_task(task_id=task_id, updated_task=updated_task, db=db)
+
+"""Modify part of task by id"""
+@router.patch("/tasks/{task_id}", response_model=task_schemas.Task)
+def patch_task(task_id: int, updated_task: task_schemas.TaskUpdate, db: Session = Depends(get_db)):
+    return task_crud.patch_task(task_id=task_id, updated_task=updated_task, db=db)
